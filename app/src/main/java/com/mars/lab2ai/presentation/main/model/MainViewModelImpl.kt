de397a3772dcd25caf1f3b1f53dc743fd58bda3e
Mallivance <ashpek01@gmail.com>
2021-04-01 12:09:32 +0300

    Improve number input validation

--- app/src/main/java/com/mars/lab2ai/presentation/main/model/MainViewModelImpl.kt
+++ app/src/main/java/com/mars/lab2ai/presentation/main/model/MainViewModelImpl.kt
@@ -4,41 +4,69 @@ import androidx.lifecycle.MutableLiveData
 
 class MainViewModelImpl : MainViewModel() {
 
+    companion object {
+        private const val DOT = '.'
+    }
+
     override val isUValidLiveData: MutableLiveData<Boolean> = MutableLiveData(false)
 
     private val u: Array<String> = Array(6) { "" }
 
     override fun setU1(u1: String) {
         u[0] = u1
-        validateU()
+        validateUI()
     }
 
     override fun setU2(u1: String) {
         u[1] = u1
-        validateU()
+        validateUI()
     }
 
     override fun setU3(u1: String) {
         u[2] = u1
-        validateU()
+        validateUI()
     }
 
     override fun setU4(u1: String) {
         u[3] = u1
-        validateU()
+        validateUI()
     }
 
     override fun setU5(u1: String) {
         u[4] = u1
-        validateU()
+        validateUI()
     }
 
     override fun setU6(u1: String) {
         u[5] = u1
-        validateU()
+        validateUI()
     }
 
-    private fun validateU() {
-        isUValidLiveData.value = u.all { it.isNotBlank() }
+    private fun validateUI() {
+        isUValidLiveData.value = u.all { it.isNotBlank() && it.isValidNumber() }
+    }
+
+    private fun CharSequence.isValidNumber(): Boolean {
+        var hadComma = false
+        var isCommaLast = false
+        val result = all {
+            when {
+                it.isDigit() -> {
+                    isCommaLast = false
+                    true
+                }
+                it == DOT -> {
+                    if (hadComma) false
+                    else {
+                        hadComma = true
+                        isCommaLast = true
+                        true
+                    }
+                }
+                else -> false
+            }
+        }
+        if (isCommaLast) return false
+        return result
     }
 }
