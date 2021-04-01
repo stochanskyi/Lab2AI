e15da65589852e7901a3667830dd3317a747042c
Marian Stochanskyi <stochan.andr@gmail.com>
2021-04-01 12:03:12 +0300

    implement handling U values

--- app/src/main/java/com/mars/lab2ai/presentation/main/MainFragment.kt
+++ app/src/main/java/com/mars/lab2ai/presentation/main/MainFragment.kt
@@ -1,14 +1,33 @@
 package com.mars.lab2ai.presentation.main
 
+import androidx.core.widget.doAfterTextChanged
 import com.mars.lab2ai.R
+import com.mars.lab2ai.databinding.FragmentMainBinding
 import com.mars.lab2ai.presentation.base.BaseFragment
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
+import org.koin.android.ext.android.bind
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : BaseFragment(R.layout.fragment_main) {
 
     private val viewModel: MainViewModel by viewModel()
 
+    private val binding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
+
     companion object {
         fun newInstance() = MainFragment()
+    }
+
+    override fun initViews() {
+        super.initViews()
+        binding.u1EditText.doAfterTextChanged { viewModel.setU1(it.toString()) }
+        binding.u2EditText.doAfterTextChanged { viewModel.setU2(it.toString()) }
+        binding.u3EditText.doAfterTextChanged { viewModel.setU3(it.toString()) }
+        binding.u4EditText.doAfterTextChanged { viewModel.setU4(it.toString()) }
+        binding.u5EditText.doAfterTextChanged { viewModel.setU5(it.toString()) }
+
+        viewModel.isUValidLiveData.observe(viewLifecycleOwner) { isConfirmEnabled ->
+            binding.confirmUButton.isEnabled = isConfirmEnabled
+        }
     }
 }
--- app/src/main/java/com/mars/lab2ai/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/mars/lab2ai/presentation/main/MainViewModel.kt
@@ -1,9 +1,59 @@
 package com.mars.lab2ai.presentation.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.mars.lab2ai.presentation.base.BaseViewModel
 
 abstract class MainViewModel : BaseViewModel() {
 
+    abstract val isUValidLiveData: LiveData<Boolean>
+
+    abstract fun setU1(u1: String)
+    abstract fun setU2(u1: String)
+    abstract fun setU3(u1: String)
+    abstract fun setU4(u1: String)
+    abstract fun setU5(u1: String)
+    abstract fun setU6(u1: String)
+
 }
 
-class MainViewModelImpl : MainViewModel()
+class MainViewModelImpl : MainViewModel() {
+
+    override val isUValidLiveData: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    private val u: Array<String> = Array(6) { "" }
+
+    override fun setU1(u1: String) {
+        u[0] = u1
+        validateU()
+    }
+
+    override fun setU2(u1: String) {
+        u[1] = u1
+        validateU()
+    }
+
+    override fun setU3(u1: String) {
+        u[2] = u1
+        validateU()
+    }
+
+    override fun setU4(u1: String) {
+        u[3] = u1
+        validateU()
+    }
+
+    override fun setU5(u1: String) {
+        u[4] = u1
+        validateU()
+    }
+
+    override fun setU6(u1: String) {
+        u[5] = u1
+        validateU()
+    }
+
+    private fun validateU() {
+        isUValidLiveData.value = u.all { it.isNotBlank() }
+    }
+}
